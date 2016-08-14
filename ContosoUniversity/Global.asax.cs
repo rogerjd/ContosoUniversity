f669c7e250391e12459dfcb01ccb01d6e17fc0a3
roger <rjpd@netzero.com>
2016-08-13 22:32:12 -0400

    moved sch initializer, std enrollments, view

--- ContosoUniversity/Global.asax.cs
+++ ContosoUniversity/Global.asax.cs
@@ -1,5 +1,4 @@
 ﻿using Entities.Context;
-using Entities.Initializer;
 using System;
 using System.Collections.Generic;
 using System.Data.Entity;
--- Entities/Context/SchoolInitializer.cs
+++ Entities/Context/SchoolInitializer.cs
@@ -1,5 +1,4 @@
 ﻿using ContosoUniversity.Models;
-using Entities.Context;
 using System;
 using System.Collections.Generic;
 using System.Data.Entity;
@@ -7,7 +6,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Entities.Initializer
+namespace Entities.Context
 {
     public class SchoolInitializer : DropCreateDatabaseIfModelChanges<SchoolContext>
     {
