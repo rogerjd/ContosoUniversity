5b4517add261d4c0286d03b9fbb622ca17c580ab
roger <rjpd@netzero.com>
2016-08-18 03:37:55 -0400

    logging

--- Entities/Logging/ILogger.cs
+++ Entities/Logging/ILogger.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
-namespace ContosoUniversity.Logging
+namespace Entities.Logging
 {
     public interface ILogger
     {
--- Entities/Logging/Logger.cs
+++ Entities/Logging/Logger.cs
@@ -6,7 +6,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
-namespace ContosoUniversity.Logging
+namespace Entities.Logging
 {
     public class Logger : ILogger
     {
