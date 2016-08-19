00e6bb438e6e22f732a523fc61cd586804503c35
roger <rjpd@netzero.com>
2016-08-18 20:35:06 -0400

    log transient error

---
+++ Entities/Context/SchoolInterceptorTransientErrors.cs
@@ -0,0 +1,54 @@
+﻿using Entities.Logging;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.Infrastructure.Interception;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Common;
+using System.Data.SqlClient;
+
+namespace Entities.Context
+{
+    class SchoolInterceptorTransientErrors : DbCommandInterceptor
+    {
+        int _counter = 0;
+        ILogger _logger = new Logger();
+
+        public override void ReaderExecuting(DbCommand command, DbCommandInterceptionContext<DbDataReader> interceptionContext)
+        {
+            bool throwTransientErrors = false;
+            if (command.Parameters.Count > 0 && command.Parameters[0].Value.ToString() == "%Throw%")
+            {
+                throwTransientErrors = true;
+                command.Parameters[0].Value = "%an%";
+                command.Parameters[1].Value = "%an%";
+            }
+
+            if (throwTransientErrors && _counter < 4)
+            {
+                _logger.Information("Returning transient error for command {0}", command.CommandText);
+                _counter++;
+                interceptionContext.Exception = CreateDummySqlException();
+            }
+            base.ReaderExecuting(command, interceptionContext);
+        }
+
+        SqlException CreateDummySqlException()
+        {
+            var sqlErrorNumber = 20;
+
+            var sqlErrorCtor = typeof(SqlError).GetConstructors(System.Reflection.BindingFlags.Instance | System.Reflection.BindingFlags.NonPublic).Where(c => c.GetParameters().Count() == 7).Single();
+            var sqlError = sqlErrorCtor.Invoke(new object[] { sqlErrorNumber, (byte)0, (byte)0, "", "", "", 1 });
+
+            var errorCollection = Activator.CreateInstance(typeof(SqlErrorCollection), true);
+            var addMethod = typeof(SqlErrorCollection).GetMethod("Add", System.Reflection.BindingFlags.Instance | System.Reflection.BindingFlags.NonPublic);
+            addMethod.Invoke(errorCollection, new[] { sqlError });
+
+            var sqlExceptionCtor = typeof(SqlException).GetConstructors(System.Reflection.BindingFlags.Instance | System.Reflection.BindingFlags.NonPublic).Where(c => c.GetParameters().Count() == 4).Single();
+            var sqlException = (SqlException)sqlErrorCtor.Invoke(new object[] { "Dummy", errorCollection, null, Guid.NewGuid() });
+
+            return sqlException;
+        }
+    }
+}
