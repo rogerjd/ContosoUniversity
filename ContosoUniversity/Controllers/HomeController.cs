3321a6d87bf68f6c90a2018d7dcdaefe023768a1
roger <rjpd@netzero.com>
2016-09-27 17:04:18 -0400

    moved to new namespace

--- ContosoUniversity/Controllers/HomeController.cs
+++ ContosoUniversity/Controllers/HomeController.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Entities.Models;
-using Entities.ViewModels;
+using ContosoUniversity.ViewModels;
 using Entities.Context;
 
 namespace Entities.Controllers
