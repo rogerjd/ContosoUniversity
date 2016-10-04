25871c756244ddc90ae1d69ec212c3d1d8bcce3e
roger <rjpd@netzero.com>
2016-10-04 15:02:20 -0400

    UpdateCourseCredits.Validation

--- ContosoUniversity/Controllers/CourseController.cs
+++ ContosoUniversity/Controllers/CourseController.cs
@@ -38,7 +38,7 @@ namespace ContosoUniversity.Controllers
                 .Where(c => !SelectedDepartment.HasValue || c.DepartmentID == departmentID)
                 .OrderBy(d => d.CourseID)
                 .Include(d => d.Department);
-            var sql = courses.ToString();
+//debug: warning unused var suppressed if not compile time constant       var sql = courses.ToString();
             return View(courses.ToList());
         }
 
@@ -181,6 +181,11 @@ namespace ContosoUniversity.Controllers
             if (multiplier != null)
             {
                 ViewBag.RowsAffected = db.Database.ExecuteSqlCommand("UPDATE Course SET Credits = Credits * {0}", multiplier);
+            }
+            else
+            {
+                ModelState.AddModelError("", "test err");
+                ViewBag.err = "invalid entry";
             }
             return View();
         }
